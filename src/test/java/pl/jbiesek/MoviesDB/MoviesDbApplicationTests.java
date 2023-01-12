d693320706fe42620a0727da890b25c2b93c79ce
jbiesek <jedrzejbiesek@wp.pl>
2023-01-12 18:31:24 +0100

    first commit

---
+++ src/test/java/pl/jbiesek/MoviesDB/MoviesDbApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jbiesek.MoviesDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
