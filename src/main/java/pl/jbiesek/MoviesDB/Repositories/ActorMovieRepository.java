85f3a564087d949b0e63b16fc7c814468c9af92c
jbiesek <jedrzejbiesek@wp.pl>
2023-03-05 13:43:33 +0100

    Readme update and minor fixes

--- src/main/java/pl/jbiesek/MoviesDB/Repositories/ActorMovieRepository.java
+++ src/main/java/pl/jbiesek/MoviesDB/Repositories/ActorMovieRepository.java
@@ -7,7 +7,7 @@ import pl.jbiesek.MoviesDB.Entities.ActorMovie;
 
 @Repository
 public interface ActorMovieRepository extends JpaRepository<ActorMovie, Integer> {
-    @Query(value = "select role from actor_movie where actor_id = 1 and movie_id = 1", nativeQuery = true)
+    @Query(value = "select role from actor_movie where actor_id = :id1 and movie_id = :id2", nativeQuery = true)
     String getRoleByActorAndMovie (int id1, int id2);
 }
 
